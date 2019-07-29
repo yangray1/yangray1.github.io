ce43e61fea73f89aa040763f3c0169664d5d791c
Raymond Yang <raaymond.yang@mail.utoronto.ca>
2019-07-29 00:38:57 -0400

    Testing with preloading part 2

--- js/index.js
+++ js/index.js
@@ -31,6 +31,7 @@ $('.scrollDownButton').on('click', (e) => {
 const nbseHacksImage = document.querySelector('.nsbeHacksImage');
 const nbseHacksCard = document.querySelector('.nsbeHacksCard');
 nbseHacksCard.addEventListener('mouseover', () => {
+    // nbseHacksImage.src = "https://media.giphy.com/media/MDfuWQGDK6AYLBuCde/giphy.gif";
     if (!nbseHacksImage.src.includes("hoverClips/NSBEHacksClip.gif")){
         nbseHacksImage.src = "hoverClips/NSBEHacksClip.gif";
     }
@@ -39,13 +40,37 @@ nbseHacksCard.addEventListener('mouseleave', () => {
     nbseHacksImage.src = "images/nsbeHacks.png";
 })
 
-const RUHacksImage = document.querySelector('.RUHacksImage');
+// const RUHacksImage = document.querySelector('.RUHacksImage');
+const RUHacksVideo = document.querySelector('.RUHacksImage2');
 const RUHacksCard = document.querySelector('.RUHacksCard');
+
 RUHacksCard.addEventListener('mouseover', () => {
-    if (!RUHacksImage.src.includes("hoverClips/RUHacksClip.gif")){
-        RUHacksImage.src = "hoverClips/RUHacksClip.gif";
+    // RUHacksImage.src = "https://www.dropbox.com/s/d20btgpepsqz8qv/RUHacksClip.gif?dl=1";
+    if (!RUHacksVideo.src.includes("hoverClips/RUHacksClip.mp4")){
+
+        // play the video
+        RUHacksVideo.src = "hoverClips/RUHacksClip.mp4";
+        RUHacksVideo.get(0).play();
+
+        // Set the default img to not appear
+        RUHacksVideo.poster= "";
+        RUHacksVideo.width = $('.RUHacksCard').width;
     }
 })
 RUHacksCard.addEventListener('mouseleave', () => {
-    RUHacksImage.src = "images/teamJelly.png";
+    // Clear the video and put back the default img
+    RUHacksVideo.src = "";
+    RUHacksVideo.poster = "images/teamJelly.png";
 })
+
+// /* RU Hacks hover clip */
+// const RUHacksImage = document.querySelector('.RUHacksImage');
+// const RUHacksCard = document.querySelector('.RUHacksCard');
+// RUHacksCard.addEventListener('mouseover', () => {
+//     if (!RUHacksImage.src.includes("hoverClips/RUHacksClip.gif")){
+//         RUHacksImage.src = "hoverClips/RUHacksClip.gif";
+//     }
+// })
+// RUHacksCard.addEventListener('mouseleave', () => {
+//     RUHacksImage.src = "images/teamJelly.png";
+// })
